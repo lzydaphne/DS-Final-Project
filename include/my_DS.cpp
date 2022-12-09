296d072123c158a4c12ce3fa5d052fd724529e27
lzydaphne <52618403+lzydaphne@users.noreply.github.com>
2022-12-09 14:48:15 +0800

    1209

--- include/my_DS.cpp
+++ include/my_DS.cpp
@@ -2,25 +2,31 @@
 
 using namespace std;
 
-// ------------------- Graph -------------------
+//! ------------------- Graph -------------------
 
-void my_graph::initial_graph(int v){
+// adjacency matrix
+void my_graph::initial_graph(int v)
+{
     V = v;
-    graph = new int*[V];
-    for (int i = 0; i < V; i++){
+    graph = new int *[V];
+    for (int i = 0; i < V; i++)
+    {
         graph[i] = new int[V];
         for (int j = 0; j < V; j++)
-            graph[i][j] = 0;  // initialization, 0 stand for disconnected
+            graph[i][j] = 0; // initialization, 0 stand for disconnected
     }
 }
 
-void my_graph::add_edge(int s, int e, int w){
+void my_graph::add_edge(int s, int e, int w)
+{
     graph[s][e] = w;
     graph[e][s] = w;
 }
 
-void my_graph::print_graph(){
-    for (int i = 0; i < V; i++){
+void my_graph::print_graph()
+{
+    for (int i = 0; i < V; i++)
+    {
         for (int j = 0; j < V; j++)
             cout << graph[i][j] << " ";
         cout << endl;
@@ -33,78 +39,77 @@ int my_graph::minDistance(int dist[], bool sptSet[])
 {
     // Initialize min value
     int min = 100000, min_index;
- 
+
     for (int v = 0; v < V; v++)
         if (sptSet[v] == false && dist[v] <= min)
             min = dist[v], min_index = v;
- 
+
     return min_index;
 }
- 
+
 // Function that implements Dijkstra's single source shortest path algorithm
 // for a graph represented using adjacency matrix representation
 int my_graph::dijkstra(int src, int des)
 {
     int dist[V]; // The output array.  dist[i] will hold the shortest
     // distance from src to i
- 
+
     bool sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest
     // path tree or shortest distance from src to i is finalized
- 
+
     // Initialize all distances as INFINITE and stpSet[] as false
     for (int i = 0; i < V; i++)
         dist[i] = 100000, sptSet[i] = false;
- 
+
     // Distance of source vertex from itself is always 0
     dist[src] = 0;
- 
+
     // Find shortest path for all vertices
-    for (int count = 0; count < V - 1; count++) {
+    for (int count = 0; count < V - 1; count++)
+    {
         // Pick the minimum distance vertex from the set of vertices not
         // yet processed. u is always equal to src in the first iteration.
         int u = minDistance(dist, sptSet);
- 
+
         // Mark the picked vertex as processed
         sptSet[u] = true;
- 
+
         // Update dist value of the adjacent vertices of the picked vertex.
         for (int v = 0; v < V; v++)
- 
+
             // Update dist[v] only if is not in sptSet, there is an edge from
             // u to v, and total weight of path from src to  v through u is
             // smaller than current value of dist[v]
-            if (!sptSet[v] && graph[u][v] && dist[u] != 100000
-                && dist[u] + graph[u][v] < dist[v])
+            if (!sptSet[v] && graph[u][v] && dist[u] != 100000 && dist[u] + graph[u][v] < dist[v])
                 dist[v] = dist[u] + graph[u][v];
     }
 
     return dist[des];
 }
 
+//! ------------------- Min Heap -------------------
 
-// ------------------- Min Heap -------------------
-
-void my_MinHeap::insertKey(int k){
+void my_MinHeap::insertKey(int k)
+{
     if (heap_size == capacity)
     {
         cout << "\nOverflow: Could not insertKey\n";
         return;
     }
-  
+
     // First insert the new key at the end
     heap_size++;
     int i = heap_size - 1;
     harr[i] = k;
-  
+
     // Fix the min heap property if it is violated
     while (i != 0 && harr[parent(i)] > harr[i])
     {
-       swap(&harr[i], &harr[parent(i)]);
-       i = parent(i);
+        swap(&harr[i], &harr[parent(i)]);
+        i = parent(i);
     }
 
     harr[100] = heap_size;
-
 }
 
 void my_MinHeap::MinHeapify(int i)
@@ -134,13 +139,13 @@ int my_MinHeap::extractMin()
     {
         // cout << "about to empty !!!!!!" << endl;
         heap_size--;
-        harr[100] = heap_size;  // don't forget to update heap size
+        harr[100] = heap_size; // don't forget to update heap size
         return harr[0];
     }
-  
+
     // Store the minimum value, and remove it from heap
     int root = harr[0];
-    harr[0] = harr[heap_size-1];
+    harr[0] = harr[heap_size - 1];
     heap_size--;
     MinHeapify(0);
 
@@ -148,18 +153,21 @@ int my_MinHeap::extractMin()
     return root;
 }
 
-bool my_MinHeap::isEmpty(){
+bool my_MinHeap::isEmpty()
+{
     // cout << "heap size: " << heap_size << endl;
-    if(heap_size == 0)
+    if (heap_size == 0)
         return true;
     else
         return false;
 }
 
-void my_MinHeap::printHeapSort(ofstream& ofs_){  // use reference to write the same output file in main
+void my_MinHeap::printHeapSort(ofstream &ofs_)
+{ // use reference to write the same output file in main
     // cout << "heap size: " << heap_size << endl;
     int min;
-    while(heap_size != 0){
+    while (heap_size != 0)
+    {
         min = extractMin();
         cout << min << " ";
         ofs_ << min << " ";
--- include/my_data.cpp
+++ include/my_data.cpp
@@ -1,106 +1,136 @@
 #include "my_data.h"
 
-int my_data::get_station_num(){
+int my_data::get_station_num()
+{
     ifstream ifs("./test_case/station.txt", ios::in);
-    if (!ifs.is_open()) {
+    if (!ifs.is_open())
+    {
         cout << "Failed to open station file.\n";
         return 0;
     }
-    else {
+    else
+    {
         string s;
-        while (std::getline(ifs, s)){
-            station_num ++;
+        while (std::getline(ifs, s))
+        {
+            station_num++;
         }
     }
     ifs.close();
     return station_num;
 }
 
-my_graph* my_data::read_map(){
+my_graph *my_data::read_map()
+{
     // read station data
     ifstream ifs("./test_case/map.txt", ios::in);
-    if (!ifs.is_open()) {
+    if (!ifs.is_open())
+    {
         cout << "Failed to open map file.\n";
         return 0;
     }
-    else {
-        my_graph* graph_ptr = new my_graph();  // create an object pointer
+    else
+    {
+        my_graph *graph_ptr = new my_graph(); // create an object pointer
         graph_ptr->initial_graph(station_num);
-        while (ifs >> start_station >> end_station >> distance) {   // overloading >> operator?
+        while (ifs >> start_station >> end_station >> distance)
+        { // overloading >> operator?
             // add edges into graph
-            graph_ptr->add_edge(start_station-1, end_station-1, distance); // station id start from 0
+            graph_ptr->add_edge(start_station - 1, end_station - 1, distance); // station id start from 0
             // cout << "start: "<< start_station-1 << " end: " << end_station-1 << " distance: " << distance << "\n";
         }
         // graph_ptr->print_graph();
         // cout << "min distance: " << graph_ptr->dijkstra(0, 5) << endl;
         ifs.close();
-        return graph_ptr;  // return the object pointer
+        return graph_ptr; // return the object pointer
     }
 }
 
-my_station** my_data::read_station(){
+//! 這邊在讀station，用一個station_num大小的array，裡面儲存
+my_station **my_data::read_station()
+{
     ifstream ifs("./test_case/station.txt", ios::in);
-    if (!ifs.is_open()) {
+    if (!ifs.is_open())
+    {
         cout << "Failed to open station file.\n";
         return 0;
     }
-    else {
-        my_station** station_ptr = new my_station*[station_num];  // station_ptr point to an array, and the array will store each station's object pointer
-        while (ifs >> station_id >> num_elec >> num_lady >> num_road) {
-            // add bikes_id into priority queue (inplemented by min heap) respectively
-            my_station* ms = new my_station();
-            my_MinHeap mm_elec(ms->electric, 0);   // sent in a pointer point to its min heap (there will be multiple min heap), and the initial heap size
+    else
+    {
+        my_station **station_ptr = new my_station *[station_num]; // station_ptr point to an array, and the array will store each station's object pointer
+        //! 每一站分別有多少台不同種類的bike
+        while (ifs >> station_id >> num_elec >> num_lady >> num_road)
+        {
+            // add bikes_id into priority queue (implemented by min heap) respectively
+            my_station *ms = new my_station();
+            //* my_MinHeap(int* bike_type, int my_heap_size)
+            my_MinHeap mm_elec(ms->electric, 0); // sent in a pointer point to its min heap (there will be multiple min heap), and the initial heap size
             my_MinHeap mm_lady(ms->lady, 0);
             my_MinHeap mm_road(ms->road, 0);
-            for(int i=0; i<num_elec; i++){
-                mm_elec.insertKey(station_id*100 + i);
+            // todo : station_id * 100+i的原因?
+            // 幫每台bike加上id
+            for (int i = 0; i < num_elec; i++)
+            {
+                mm_elec.insertKey(station_id * 100 + i);
             }
-            for(int i=0; i<num_lady; i++){
-                mm_lady.insertKey(station_id*100 + i);
+            for (int i = 0; i < num_lady; i++)
+            {
+                mm_lady.insertKey(station_id * 100 + i);
             }
-            for(int i=0; i<num_road; i++){
-                mm_road.insertKey(station_id*100 + i);
+            for (int i = 0; i < num_road; i++)
+            {
+                mm_road.insertKey(station_id * 100 + i);
             }
-            station_ptr[station_id-1] = ms; // station id start from 0, storing the station's object pointer
+            station_ptr[station_id - 1] = ms; // station id start from 0, storing the station's object pointer
         }
         ifs.close();
         return station_ptr;
     }
 }
 
-void my_data::read_fee(){
+void my_data::read_fee()
+{
     ifstream ifs("./test_case/fee.txt", ios::in);
-    if (!ifs.is_open()) {
+    if (!ifs.is_open())
+    {
         cout << "Failed to open fee file.\n";
     }
-    else {
+    else
+    {
         int count = 0;
-        while (!ifs.eof()) {
+        while (!ifs.eof())
+        {
             // store the fee information
-            if(count < 3){  // the first 3 lines
+            if (count < 3)
+            { // the first 3 lines
                 ifs >> bike_type >> discount[count] >> regular[count];
             }
-            else{
+            else
+            {
                 ifs >> waiting;
                 ifs >> switching;
                 ifs >> transferring;
             }
-            count ++;
+            count++;
         }
     }
     ifs.close();
 }
 
-int my_data::read_user(){
+int my_data::read_user()
+{
     ifstream ifs("./test_case/user.txt", ios::in);
-    if (!ifs.is_open()) {
+    if (!ifs.is_open())
+    {
         cout << "Failed to open user file.\n";
         return 0;
     }
-    else {
+    else
+    {
         string s;
-        while (std::getline(ifs, s)){
-            user_num ++;
+        while (std::getline(ifs, s))
+        {
+            user_num++;
         }
     }
     ifs.close();
